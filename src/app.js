5b760f9f299bf6244fa0f4ef1dfac062d3073766
MuhammadWaleedMalik <148714412+MuhammadWaleedMalik@users.noreply.github.com>
2024-12-22 16:22:26 +0500

    FRONT END PART STARTED

--- src/app.js
+++ src/app.js
@@ -1,47 +1,35 @@
 import express from "express";
-import cors from 'cors'
+import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path"; 
 
+const app = express();
 
-
-const app=express()
-
-
+// Middleware
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials:true
-}))
+    credentials: true
+}));
 
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static(path.join(path.resolve(), "public"))); 
+app.use(cookieParser());
 
-app.use(express.json({limit:'16kb'}))
-app.use(express.urlencoded({extended:true,limit:'16kb'}))
-app.use(express.static('public'))
-app.use(cookieParser())
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(path.resolve(), "views"));
+ 
 
+// Import Routes
+import userRouter from "./routes/user.routes.js";
 
-//Routes import 
+// Routes
+app.use("/api/v1/users", userRouter);
 
-import userRouter from './routes/user.routes.js'
+// Basic route for testing EJS
+app.get("/", (req, res) => {
+    res.render("pages/home");
+});
 
-
-
-
-
-
-
-
-
-
-
-// Routes Decleration
-app.use('/api/v1/users',userRouter)
-
-
-
-
-
-
-
-
-
-export {app}
+export { app };
